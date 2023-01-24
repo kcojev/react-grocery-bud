25e57bec14c68ee97fb325f0558f02c44af10bbf
Georgy Kcojev <kcojev@gmail.com>
2023-01-24 11:54:06 +0300

    List component setup and add item functionality

--- src/App.js
+++ src/App.js
@@ -8,9 +8,21 @@ function App() {
   const [isEditing, setIsEditing] = useState(false)
   const [editID, setEditID] = useState(null)
   const [alert, setAlert] = useState({show: false, msg: '', type: ''})
+  
   const handleSubmit = (e) => {
     e.preventDefault()
-    console.log('hello');
+    if(!name) {
+      // display alert
+    }
+    else if(name && isEditing) {
+      // deal with edit
+    }
+    else {
+      // show alert
+      const newItem = {id: new Date().getTime().toString(), title: name}
+      setList([...list, newItem])
+      setName('')
+    }
   }
 
   return (
@@ -31,12 +43,15 @@ function App() {
           </button>
         </div>
       </form>
-      <div className="grocery-container">
-        <List />
-        <button className="clear-btn">
-          clear items
-        </button>
-      </div>
+      {list.length > 0 && (
+        <div className="grocery-container">
+          <List items={list}/>
+          <button className="clear-btn">
+            clear items
+          </button>
+        </div>
+      )}
+      
     </section>
   )
 }
